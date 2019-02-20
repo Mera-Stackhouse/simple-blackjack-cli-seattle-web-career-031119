377f5168bd09608c4fff3070903fa9df13d4d4e3
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 06:50:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,17 +37,23 @@ def initial_round
   card_total
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
   prompt_user
   input = get_user_input
   if input == 'h'
     deal_card
+    card_total = card_total + deal_card
+  elsif input == 's'
+    card_total
+  else
+    invalid_command
   end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
