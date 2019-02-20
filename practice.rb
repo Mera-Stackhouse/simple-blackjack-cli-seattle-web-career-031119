9deecfecc05e4400f924ae5434e7d096ed23319b
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 07:01:17 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,9 +1,6 @@
-def hit?(card_total)
-  # code hit? here
-  prompt_user
-  input = get_user_input
+def hit?(card_total, input)
   if input == 'h'
-    new_card = deal_card
+    new_card = 2
     new_total = card_total + new_card
   elsif input == 's'
     card_total
@@ -12,3 +9,5 @@ def hit?(card_total)
     card_total
   end
 end
+
+hit?(10, 'h')
