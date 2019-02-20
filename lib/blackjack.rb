b7130cf8435185649ed0ea92459dd23f9b2afbbd
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 06:37:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry you lose. Your card total was #{card_total}. Thank you for playing"
 end
 
 def initial_round
