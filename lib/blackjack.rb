02462ff956ddc5022e81f6b1637ba63efe6015e9
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 06:51:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,12 +48,14 @@ def hit?(card_total)
     card_total
   else
     invalid_command
+    card_total
   end
 end
 
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
