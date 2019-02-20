200a30401eed57c53454ddfb7669a735595c4b51
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 06:58:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,7 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
   if input == 'h'
-    card_total += deal_card
+    new total = card_total + deal_card
   elsif input == 's'
     card_total
   else
